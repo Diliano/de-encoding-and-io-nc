ba9eacd79aa1beb326e5259ec9af372262131095
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-23 15:35:03 +0100

    create and add functionality for the get_cars_under_20_years_old() function, for use with the cars.json data

--- cars.py
+++ cars.py
@@ -11,3 +11,10 @@ def get_car_makes(car_data):
     return {car_dict["make"] for car_dict in car_data}
 
 # print(get_car_makes(car_data))
+
+def get_cars_under_20_years_old(car_data):
+    cars_under_20 = \
+    [car_dict for car_dict in car_data if (2024 - car_dict["year"]) < 20]
+    return sorted(cars_under_20, key=lambda x: x["year"], reverse=True)
+
+# print(get_cars_under_20_years_old(car_data))
