6a406507b06a79ca5a5594632a15fb45d41578f9
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-23 11:48:49 +0100

    create and add functionality for the get_pizzas_under_9() function

--- pizza.py
+++ pizza.py
@@ -15,3 +15,9 @@ def get_ingredients(list_of_pizza_dicts, pizza):
         
 capri_ingredients = get_ingredients(pizza_dicts, "Capri")
 print(capri_ingredients)
+
+def get_pizzas_under_9(list_of_pizza_dicts):
+    pizzas_under_9 = [pizza_dict for pizza_dict in list_of_pizza_dicts if float(pizza_dict["Cost"][1:]) < 9]
+    return sorted(pizzas_under_9, key=lambda x: float(x["Cost"][1:]))
+
+print(len(get_pizzas_under_9(pizza_dicts)))
