be4d9fa820d9eaae20a54afb1fbd246698705e57
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-23 11:32:46 +0100

    refactored get_ingredients() to handle additional unwanted spaces

--- pizza.py
+++ pizza.py
@@ -9,9 +9,9 @@ pizza_dicts = csv_to_dict("./data/pizza.csv")
 print(pizza_dicts)
 
 def get_ingredients(list_of_pizza_dicts, pizza):
-    for dict in list_of_pizza_dicts:
-        if dict["Pizza"] == pizza:
-            return dict["Description"].split(",")
+    for pizza_dict in list_of_pizza_dicts:
+        if pizza_dict["Pizza"] == pizza:
+            return pizza_dict["Description"].split(", ")
         
 capri_ingredients = get_ingredients(pizza_dicts, "Capri")
 print(capri_ingredients)
