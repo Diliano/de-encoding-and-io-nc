4db91648156ced14e88724e30ea07046adfd0442
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-23 11:22:12 +0100

    create and add functionality for the get_ingredients() function, which returns a list of all the ingredients of a given pizza

--- pizza.py
+++ pizza.py
@@ -5,4 +5,13 @@ def csv_to_dict(filepath, encoding="utf-8"):
         reader = csv.DictReader(f)
         return list(reader)
     
-print(csv_to_dict("./data/pizza.csv"))
+pizza_dicts = csv_to_dict("./data/pizza.csv")
+print(pizza_dicts)
+
+def get_ingredients(list_of_pizza_dicts, pizza):
+    for dict in list_of_pizza_dicts:
+        if dict["Pizza"] == pizza:
+            return dict["Description"].split(",")
+        
+capri_ingredients = get_ingredients(pizza_dicts, "Capri")
+print(capri_ingredients)
