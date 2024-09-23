2fe6e3d250ea25d83311cc919781430720302c3e
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-23 15:20:45 +0100

    create and add functionality for the find_car_makes() function, for use with the cars.json data

--- cars.py
+++ cars.py
@@ -4,4 +4,10 @@ def json_to_list_of_dicts(filepath):
     with open(filepath, encoding="utf-16") as f:
         return json.load(f)["Cars"]
     
-# print(json_to_list_of_dicts("./data/cars.json"))
+car_data = json_to_list_of_dicts("./data/cars.json")
+# print(car_data)
+
+def find_car_makes(car_data):
+    return {car_dict["make"] for car_dict in car_data}
+
+# print(find_car_makes(car_data))
